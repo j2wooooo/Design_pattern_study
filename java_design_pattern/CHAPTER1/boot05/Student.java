e12a6a55fd86d496727c383ac294d232e75a2a39
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-09 13:33:53 +0900

    단방향 연관 관계

--- java_design_pattern/CHAPTER1/boot05/Student.java
+++ java_design_pattern/CHAPTER1/boot05/Student.java
@@ -30,4 +30,5 @@ public class Student {
 /* 이 코드는 Student 객체 하나에 하나 이상의 Course 객체가 연관되어 있기 때문에 다중성을 구현했으며,
   Student 클래스에 대표적 컬렉션 자료구조인 Vector를 이용해 여러 개의 Course 클래스 객체를 참조할 수 있게 했다.
   컬렉션 자료구조에는 Vector 외에도 Set, Map, ArrayList 등 여러 가지가 있으므로 상황에 맞는 적절한 자료구조를 선택해 사용하면 된다.
-  또한 이 코드는 Student 클래스에서 Course 클래스로 향하는 단방향 연관 관계이기 때문에 Course 클래스에서는 Student 객체를 참조하는 속성이 정의되어 있지 않다. */
+  또한 이 코드는 Student 클래스에서 Course 클래스로 향하는 단방향 연관 관계이기 때문에
+  Course 클래스에서는 Student 객체를 참조하는 속성이 정의되어 있지 않다. */
