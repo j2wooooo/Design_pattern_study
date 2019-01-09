cde4a9cce051345f3195e11f1773f96b5d32f5d6
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-09 13:01:35 +0900

    Create Course.java

---
+++ java_design_pattern/CHAPTER1/boot05/Course.java
@@ -0,0 +1,14 @@
+/* [p.45] 28쪽, 그림 1-11을 코드로 작성하라. */
+package java_design_pattern;
+
+public class Course{
+	private String name;
+	
+	public Course(String name) {
+		this.name = name;
+	}
+	
+	public String getName() {
+		return name;
+	}
+}
