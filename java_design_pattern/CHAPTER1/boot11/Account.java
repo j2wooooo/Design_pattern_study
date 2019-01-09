0d52eb1800e56d220020fe91427b2d7d84445f3a
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-09 16:50:46 +0900

    Create Account.java

---
+++ java_design_pattern/CHAPTER1/boot11/Account.java
@@ -0,0 +1,12 @@
+/* [p.55] 다음 클래스 다이어그램dp 해당하는 코드를 작성하라. */
+package java_design_pattern;
+
+public class Account {
+	private int id;
+	private String owner;
+	private double balance;
+	
+	public void deposit(double amount) {};
+	public void withdraw(double amount) {};
+	protected void getBalance() {};
+}
