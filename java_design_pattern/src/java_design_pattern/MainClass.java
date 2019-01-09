05152bcd0d171f3e839a7cbff45df6ceae1c9d76
parkjiwoo <j2wooooo@naver.com>
2019-01-09 10:53:35 +0900

    java example code commit

---
+++ java_design_pattern/src/java_design_pattern/MainClass.java
@@ -0,0 +1,12 @@
+package java_design_pattern;
+
+public class MainClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			new Course();
+			
+			return;
+	}
+
+}
