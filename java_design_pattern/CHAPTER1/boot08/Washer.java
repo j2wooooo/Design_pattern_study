f4e0db37b388c402e214ef3243c9a6938264ba81
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-09 16:06:45 +0900

    Create Washer.java

---
+++ java_design_pattern/CHAPTER1/boot08/Washer.java
@@ -0,0 +1,19 @@
+/* [p.51] 35쪽, 다음 클래스 다이어그램을 코드로 작성하라. */
+package java_design_pattern;
+
+// HomeAppliancies의 메서드를 상속받음
+public class Washer extends HomeAppliancies{
+
+	public void turnOn()
+	{
+		// 세탁기를 켜는 코드
+		
+	}
+	
+	public void turnOff()
+	{
+		// 세탁기를 끄는 코드
+		
+	}
+}
+/* 나머지 클래스 Freeze와 DishWasher 모두 Washer와 같다 */
