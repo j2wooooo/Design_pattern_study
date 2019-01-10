8f2e98e7c0d89b30246a80de507f63f74ae2704c
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-10 16:05:13 +0900

    Create Queue.java

---
+++ java_design_pattern/CHAPTER2/boot07/boot07_problem_code/Queue.java
@@ -0,0 +1,23 @@
+/* 101쪽, 괄호 안을 채워 LinkedList 클래스를 이용하는 Queue 클래스를 작성하라. */
+
+package java_design_pattern;
+
+import java.util.LinkedList;
+
+public class Queue<String> extends LinkedList<String>
+{
+	public boolean addQueue(String element)
+	{
+		
+	}
+	
+	public String removeQueue()
+	{
+		
+	}
+	
+	public String peekQueue()
+	{
+		
+	}
+}
