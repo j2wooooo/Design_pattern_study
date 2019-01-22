f9b872f99bb60714a1040444362d49c297f67114
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-22 09:54:26 +0900

    Create Atom.java

---
+++ java_design_pattern/CHAPTER5/boot01/Atom.java
@@ -0,0 +1,11 @@
+/* 174쪽, 로봇만들기 코드 5-3 [개선된 코드 interface 이용] */
+
+package java_design_pattern;
+
+public class Atom extends Robot
+{
+	private String name;
+	public Atom(String name) {
+		super(name);
+	}
+}
