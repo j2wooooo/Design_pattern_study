c48f08366a88163ddf84fd4ac7fccf33a288150e
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-22 12:39:57 +0900

    Create Client.java

---
+++ java_design_pattern/CHAPTER5/boot04/boot04_problem_code/Client.java
@@ -0,0 +1,12 @@
+/* 185쪽, 5번 다음 코드와 실행 결과를 참고해 물음에 답하라. */
+
+/* 만약 프로그램이 새로운 달리기 이미지를 처리해야 한다면 이때 어떤 문제가 발생하는가? */
+
+package java_design_pattern;
+
+public class Client{
+	public static void main(String[] args)
+	{
+		new Running();
+	}
+}
