d642f107a1b2c625578c5b80eeda9df9da66db32
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-22 13:22:03 +0900

    Create MovingStrategy.java

---
+++ java_design_pattern/CHAPTER5/boot04/boot04_answer_code02/MovingStrategy.java
@@ -0,0 +1,37 @@
+/* 185쪽, 5번 다음 코드와 실행 결과를 참고해 물음에 답하라. */
+
+/* 만약 프로그램이 새로운 달리기 이미지를 처리해야 한다면 이때 어떤 문제가 발생하는가? */
+
+/* 추가적인 개선사항
+ * drawImage 메서드의 실행 흐름이 RunningStrategy나 WalkingStrategy 클래스와 거의 같다.
+ * 이 점을 이용해 다음과 같이 MovingStrategy라는 추상 클래스에 공통적인 실행 흐름을 가진 drawImage 메서드를 정의해 코드의 중복을 없앤다.
+ * 
+ * 중요한 점은 이미지 처리에서 다른 부분은 모두 protected 접근 제어자로 선언한 메서드로 대치해 자식 클래스에서 재정의할 수 있게 했다는 점이다.
+ * 이와 같이 실행 흐름은 동일하지만 어느 특정 실행 단계의 구현을 자식 클래스에서 결정하도록 함으로써 서로 다른 자식 클래스가 서로 다른 구현을 실행해 다른 기능이 실행되도록 한다.
+ * 이와 같이 동일한 흐름을 지닌 메서드의 중복을 없애고 자식 클래스에서 각 실행 흐름의 내용을 결정하도록 하는 디자인 패턴을 템플릿 메서드 패턴이라고 한다.
+  */ 
+
+package java_design_pattern;
+
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
+
+public abstract class MovingStrategy {
+	public void drawImages(JLabel lbl)
+	{
+		String[] path = getImageNames();
+		lbl.setIcon(getImageIcon(path[0]));
+		while(true)
+		{
+			for(int i = 0; i < path.length; i++)
+			{
+				lbl.setIcon(getImageIcon(path[i]));
+				try {
+					Thread.sleep(100);
+				}catch(InterruptedException e) {}
+			}
+		}
+	}
+	protected abstract ImageIcon getImageIcon(String name);
+	protected abstract String[] getImageNames();
+}
