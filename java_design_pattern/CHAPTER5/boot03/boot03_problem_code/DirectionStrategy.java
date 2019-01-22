ed4d3b11328a6e2c1744186ca4239b148f2145c0
j2wooooo <32832642+j2wooooo@users.noreply.github.com>
2019-01-22 10:43:47 +0900

    Create DirectionStrategy.java

---
+++ java_design_pattern/CHAPTER5/boot03/boot03_problem_code/DirectionStrategy.java
@@ -0,0 +1,12 @@
+/* 181쪽, 4번 다음 코드는 아래에 설명하는 요구사항을 만족하는 프로그램을 작성한 것이다. 질문에 답하라. */
+
+/* 필드 안에 2개 이상의 공이 존재한다.
+ * 각 공은 첫째, 좌우로 움직이거나 둘째, 상하로 움직이거나 셋째, 대각선으로 움직일 수 있다.
+ * 각 공은 빨간색이나 파란색으로 칠해진다.
+ */
+
+package java_design_pattern;
+
+public abstract class DirectionStrategy{
+	public abstract void move(Ball ball);
+}
